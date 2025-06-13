b7e3d013ad62b10aec657b237906225fe86ce6a3
may28-1988 <may28-1988@outlook.com>
2025-06-13 19:04:42 +0530

    Added the Jump Server configuration

--- EC2/Resource/Compute/main.tf
+++ EC2/Resource/Compute/main.tf
@@ -26,6 +26,31 @@ module "EC2_Agent" {
         Module = "ADO-Agent"
       })
 
+    },
+    (var.EC2_name) = {
+      ami                    = var.EC2_ami
+      instance_type          = var.EC2_instance_type
+      vpc_security_group_ids = var.EC2_security_group_ids
+      subnet_id              = var.EC2_subnet_id
+      iam_instance_profile   = var.EC2_instance_profile
+      root_block_device = [{
+        delete_on_termination = true
+        volume_size           = var.EC2_root_volume_size
+        volume_type           = var.EC2_root_volume_type
+        encrypted             = var.EC2_root_volume_encrypted
+        kms_key_id            = var.EC2_root_volume_kms_key_id
+        iops                  = var.EC2_root_volume_iops
+        throughput            = var.EC2_root_volume_throughput
+        tags = merge(local.default_tags, {
+          Name   = "${var.EC2_name}_volume"
+          Module = "JumpServer"
+        })
+      }]
+      tags = merge(local.default_tags, {
+        Name   = var.EC2_name
+        Module = "JumpServer"
+      })
+
     }
   }
 }
--- EC2/Resource/Compute/variables.tf
+++ EC2/Resource/Compute/variables.tf
@@ -1,4 +1,8 @@
 ############################
+# Variables for EC2 ADO Agent
+############################
+
+############################
 # EC2 Variables #
 ############################
 
@@ -63,3 +67,69 @@ variable "EC2_root_volume_kms_key_id" {
   type        = string
   default     = null
 }
+/*
+############################
+# Variables for EC2 JumpServer
+############################
+
+############################
+# EC2 Variables #
+############################
+
+variable "EC2_name-02" {
+  description = "Name of the EC2 ADO Agent"
+  type        = string
+}
+variable "EC2_ami-02" {
+  description = "AMI for the EC2 ADO Agent"
+  type        = string
+}
+variable "EC2_instance_type-02" {
+  description = "Instance type for the EC2 ADO Agent"
+  type        = string
+}
+variable "EC2_security_group_ids-02" {
+  description = "Security group IDs for the EC2 ADO Agent"
+  type        = list(string)
+}
+variable "EC2_subnet_id-02" {
+  description = "Subnet ID for the EC2 ADO Agent"
+  type        = string
+}
+variable "EC2_instance_profile-02" {
+  description = "IAM instance profile for the EC2 ADO Agent"
+  type        = string
+  default     = null
+}
+############################
+# Root Volume Variables #
+############################
+
+variable "EC2_root_volume_size-02" {
+  description = "Size of the root volume in GB"
+  type        = number
+}
+variable "EC2_root_volume_type-02" {
+  description = "Type of the root volume"
+  type        = string
+}
+variable "EC2_root_volume_iops-02" {
+  description = "IOPS for the root volume"
+  type        = number
+  default     = null
+}
+variable "EC2_root_volume_throughput-02" {
+  description = "Throughput for the root volume"
+  type        = number
+  default     = null
+}
+variable "EC2_root_volume_encrypted-02" {
+  description = "Whether the root volume is encrypted"
+  type        = bool
+  default     = false
+}
+variable "EC2_root_volume_kms_key_id-02" {
+  description = "KMS key ID for the root volume encryption"
+  type        = string
+  default     = null
+}*/
