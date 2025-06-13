3dc7eb6f1c4c3930589c7911e0de2d820841f3eb
may28-1988 <may28-1988@outlook.com>
2025-06-13 14:57:01 +0530

    Updated with Assume ROle ARn instead of Profile

--- NW-Nat/version.tf
+++ NW-Nat/version.tf
@@ -14,6 +14,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-south-1"
-  profile = "default"
+  region = "ap-south-1"
+  #profile = "default"
+
+  assume_role {
+    role_arn = "arn:aws:iam::392568849431:role/TerraformAssumeRole"
+  }
 }
