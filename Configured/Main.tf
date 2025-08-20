6804a5f3b872d0d0cb134caf72d921a53fb4e62b
bunny28may1988 <bunny28may1988@gmail.com>
2025-08-20 14:02:13 +0530

    Update Main.tf with sync with Working Copy

--- Configured/Main.tf
+++ Configured/Main.tf
@@ -6,24 +6,20 @@ terraform {
       version = "~> 4.0"
     }
     azapi = {
-      source  = "azure/azapi"
-      version = "~> 2.0"
+      source  = "Azure/azapi"
+      version = "~> 2.6.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "0e772bc3-0a1a-466f-8d51-4076bdc28134"
-  tenant_id       = "1a1680cb-e128-4577-9dbe-9aa330bd7f17"
+  subscription_id = "39ac6650-eb05-4bc6-abb3-5751abf3df0e"
+  tenant_id       = "73a4c997-ac5a-4bcd-81f3-fd25589a48b7"
 }
-
-# -------------------------
-# Base infra
-# -------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "rg-devops-pool"
-  location = "Central India" #"Central India"
+  location = "Central India"
 }
 
 resource "azurerm_dev_center" "dc" {
@@ -39,33 +35,24 @@ resource "azurerm_dev_center_project" "devproj" {
   dev_center_id       = azurerm_dev_center.dc.id
 }
 
-# -------------------------
-# User-Assigned Managed Identity + RBAC
-# (Pools require UserAssigned identity)
-# -------------------------
 resource "azurerm_user_assigned_identity" "mdp_uami" {
   name                = "uami-mdp"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
 
-# Give the UAMI permissions to create/operate the underlying infra (tighten later if desired)
 resource "azurerm_role_assignment" "mdp_uami_contributor" {
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.mdp_uami.principal_id
 }
 
-# -------------------------
-# Managed DevOps Pool (public agents; all projects in org)
-# -------------------------
 resource "azapi_resource" "managed_devops_pool" {
   type      = "Microsoft.DevOpsInfrastructure/pools@2025-01-21"
   name      = "SampleDevOpsPool"
   location  = azurerm_resource_group.rg.location
   parent_id = azurerm_resource_group.rg.id
 
-  # Must be UserAssigned (SystemAssigned not supported)
   identity {
     type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.mdp_uami.id]
@@ -73,37 +60,32 @@ resource "azapi_resource" "managed_devops_pool" {
 
   body = {
     properties = {
-      # Link to Azure DevOps org; open to ALL projects
+     
       organizationProfile = {
         kind = "AzureDevOps"
         organizations = [{
-          url        = "https://dev.azure.com/3angelsinv"
+          url        = "https://dev.azure.com/kmbl-devops"
           openAccess = true
           # projects = []  # (optional) use if you want to restrict to specific projects
         }]
       }
 
       devCenterProjectResourceId = azurerm_dev_center_project.devproj.id
-
-      # Stateless ephemeral agents; Azure auto-predicts capacity
       agentProfile = {
         kind = "Stateless"
         resourcePredictionsProfile = {
-          kind = "Automatic" # Off | Manual | Automatic
+          kind = "Automatic"
         }
       }
 
-      # VM sizing/image (no networkProfile => public agents)
       fabricProfile = {
         kind = "Vmss"
-        sku = { name = "Standard_D2s_v5" #"Standard_D2ads_v5" 
+        sku = { name = "Standard_D2s_v5"
         }
         images = [
           { wellKnownImageName = "ubuntu-22.04/latest" }
         ]
       }
-
-      # Cap of concurrent agents provisioned by Azure
       maximumConcurrency = 1
     }
   }
